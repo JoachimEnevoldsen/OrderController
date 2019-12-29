d5ba139483f47590adee5caf8054e141a5d56361
Joachim <joene@itrelation.dk>
2019-12-29 21:49:41 +0100

    init

---
+++ OrderController/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using OrderController.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OrderController.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (Session["id"] == null)
+            {
+                filterContext.Result = RedirectToAction("Index", "Login");
+                return;
+            }
+
+
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+    }
+}
---
+++ OrderController/Controllers/LoginController.cs
@@ -0,0 +1,45 @@
+﻿using OrderController.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OrderController.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Autherize(OrderController.Models.users usermodel)
+        {
+
+            using (order_control db = new order_control())
+            {
+                var user = db.users.Where(u => u.name == usermodel.name && u.password == usermodel.password).FirstOrDefault();
+
+                if (user != null)
+                {
+                    Session["id"] = user.id;
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    usermodel.LoginErrorMessage = "name or password is not correct";
+                    return View("Index", usermodel);
+                }
+            }
+        }
+
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index");
+        }
+    }
+}
